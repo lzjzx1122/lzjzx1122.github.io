93c01bf177831311206e12caed514f8d739a1308
Zijun Li <122567142@qq.com>
2025-04-17 13:41:58 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,21 +1,8 @@
-from scholarly import scholarly, ProxyGenerator
-from fp.fp import FreeProxy
+from scholarly import scholarly
 import jsonpickle
 import json
 from datetime import datetime
 import os
-
-server = "zxtw02.mutdot.link"
-port = 22749
-cipher = "aes-256-gcm"
-password = "7762614b-9bad-435d-86e5-aab66a4bec6e"
-
-# 构建 ss 代理 URL
-proxy_url = f"ss://{cipher}:{password}@{server}:{port}"
-
-pg = ProxyGenerator()
-pg.SingleProxy(proxy_url)
-scholarly._ProxyGenerator__proxy = pg
 
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
